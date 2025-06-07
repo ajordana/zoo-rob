ccaa954f0e1ec99edfd0f3e2634f51d016ea9f63
JianghanZHang <jz5480@nyu.edu>
2025-06-06 20:43:07 -0400

    modified running script: Added configs

--- traj_opt/run_traj_opt.py
+++ traj_opt/run_traj_opt.py
@@ -4,7 +4,6 @@ import numpy as np
 import mujoco
 
 from hydrax.algs import CEM, MPPI, PredictiveSampling, Evosax
-from hydrax.simulation.deterministic import run_interactive
 from hydrax.tasks.cart_pole import CartPole
 from tasks.cart_pole_unconstrained import CartPoleUnconstrained
 from tasks.pusht_unconstrained import PushTUnconstrained
@@ -14,6 +13,7 @@ from hydrax.tasks.humanoid_standup import HumanoidStandup
 from hydrax.tasks.humanoid_mocap import HumanoidMocap
 from hydrax.tasks.walker import Walker
 from traj_opt_helper import traj_opt_helper
+from algs import create_algorithm
 
 # Parameters
 num_trails = 6
@@ -23,7 +23,9 @@ sigma = 0.3
 temperature = 0.1
 spline = "zero"
 horizon = 1.0
-num_knots = 100
+num_knots = 100 # set this to (horizon/mj_model.opt.time_step) will have no spline interpolation
+
+algorithms = ["MPPI", "MPPI-CMA", "CMA-ES", "PredictiveSampling", "RandomizedSmoothing"]
 
 # # CubeRotation
 # task = CubeRotation()
@@ -31,9 +33,9 @@ num_knots = 100
 # mj_data = mujoco.MjData(mj_model)
 
 # CartPole
-# task = CartPoleUnconstrained()
-# mj_model = task.mj_model
-# mj_data = mujoco.MjData(mj_model)
+task = CartPole()
+mj_model = task.mj_model
+mj_data = mujoco.MjData(mj_model)
 
 # HumanoidStandup
 # task = HumanoidStandup()
@@ -54,23 +56,34 @@ num_knots = 100
 # initial_knots = task.reference[: num_knots, 7:]
 
 
+# PushT
+# task = PushTUnconstrained()
+# mj_model = task.mj_model
+# mj_data = mujoco.MjData(mj_model)
+# mj_data.qpos = [0.1, 0.1, 1.3, 0.0, 0.0]
 
-# # PushT
-task = PushTUnconstrained()
-mj_model = task.mj_model
-mj_data = mujoco.MjData(mj_model)
-mj_data.qpos = [0.1, 0.1, 1.3, 0.0, 0.0]
+ctrl = create_algorithm(name = "CMA-ES", 
+                        task = task,
+                        num_samples = num_samples,
+                        horizon = horizon,
+                        num_knots = num_knots,
+                        spline = spline,
+                        temperature = temperature,
+                        noise = sigma)
 
-mppi = MPPI(
-    task,
-    num_samples = num_samples,
-    temperature = temperature,
-    noise_level= sigma,
-    plan_horizon= horizon,
-    spline_type=spline,
-    num_knots=num_knots
-)
-mj_model = task.mj_model
-
-to = traj_opt_helper(mppi, mj_model, mj_data)
+to = traj_opt_helper("CMA-ES", ctrl, mj_model, mj_data)
 to.trails(max_iteration=max_iterations, num_trails = num_trails)
+
+# for algorithm in algorithms:
+
+#     ctrl = create_algorithm(name = algorithm, 
+#                         task = task,
+#                         num_samples = num_samples,
+#                         horizon = horizon,
+#                         num_knots = num_knots,
+#                         spline = spline,
+#                         temperature = temperature,
+#                         noise = sigma)
+
+#     to = traj_opt_helper(algorithm, ctrl, mj_model, mj_data)
+#     to.trails(max_iteration=max_iterations, num_trails = num_trails)
